cb99fcdca1a7d8b5b178ced34b1f41b9dae1be9c
Hery.R <2001.hery@gmail.com>
2025-02-11 17:55:43 +0100

    feat : rajout des boutons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,20 +11,26 @@ export default function Home() {
   ]);
 
   const handleItemUpdate = (updatedItem: any) => {
-    setItems(items.map(item => 
+    setItems(items.map(item =>
       item.title === updatedItem.title ? updatedItem : item
     ));
   };
 
   return (
-    <>
+    <div className="flex flex-col items-center justify-center h-screen">
       {items.map((item) => (
-        <ShowItem 
-          key={item.title} 
-          item={item} 
+        <ShowItem
+          key={item.title}
+          item={item}
           onUpdate={handleItemUpdate}
         />
       ))}
-    </>
+      <div className="flex flex-row justify-center items-center">
+        <button className="text-xl bg-secondary text-card rounded-md p-2 mr-2">Save</button>
+        <button className="text-xl bg-card text-secondary rounded-md p-2 mr-2 border-2">Load</button>
+        <button className="text-xl bg-primary text-card rounded-md p-2">Clear</button>
+      </div>
+
+    </div>
   );
 }

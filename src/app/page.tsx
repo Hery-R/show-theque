ef9043239c8d7f2700eeaac22e950e094452383d
Hery.R <2001.hery@gmail.com>
2025-02-11 17:58:38 +0100

    feat : rajout du titre

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,20 +17,27 @@ export default function Home() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      {items.map((item) => (
-        <ShowItem
-          key={item.title}
-          item={item}
-          onUpdate={handleItemUpdate}
-        />
-      ))}
-      <div className="flex flex-row justify-center items-center">
-        <button className="text-xl bg-secondary text-card rounded-md p-2 mr-2">Save</button>
-        <button className="text-xl bg-card text-secondary rounded-md p-2 mr-2 border-2">Load</button>
-        <button className="text-xl bg-primary text-card rounded-md p-2">Clear</button>
+    <>
+      <h1 className="text-3xl text-accent font-bold text-center mt-10">
+        ShowThèque
+      </h1>
+
+      <div className="flex flex-col items-center justify-center h-screen">
+        {items.map((item) => (
+          <ShowItem
+            key={item.title}
+            item={item}
+            onUpdate={handleItemUpdate}
+          />
+        ))}
+        <div className="flex flex-row justify-center items-center">
+          <button className="text-xl bg-secondary text-card rounded-md p-2 mr-2">Save</button>
+          <button className="text-xl bg-card text-secondary rounded-md p-2 mr-2 border-2">Load</button>
+          <button className="text-xl bg-primary text-card rounded-md p-2">Clear</button>
+        </div>
+
       </div>
 
-    </div>
+    </>
   );
 }
